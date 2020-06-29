1a64d65b45057ea5686ef0fc7bb6bef2ada7cc78
TianyuanDeng <dty469783894@gmail.com>
2020-06-29 17:13:10 -0500

    Linkedlist 基础互换操作

--- src/LinkedList/SwapNodesInPair_24.java
+++ src/LinkedList/SwapNodesInPair_24.java
@@ -2,6 +2,16 @@ package LinkedList;
 
 import java.util.List;
 
+/**
+ *Given a linked list, swap every two adjacent nodes and return its head.
+ *You may not modify the values in the list's nodes, only nodes itself may be changed.
+ *
+ * Example:
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ *
+ * Linkedlist 基础互换next操作；
+ */
+
 public class SwapNodesInPair_24 {
      public ListNode swapPairs(ListNode head) throws Exception {
         if (head == null || head.next == null) {
