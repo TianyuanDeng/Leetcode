12ed03b8d1c7d88b6aef96e620fef1b81331030d
TianyuanDeng <dty469783894@gmail.com>
2020-07-01 16:22:48 -0500

    Linkedlist 反转操作总结

--- src/LinkedList/ReverseLinkedListII_92.java
+++ src/LinkedList/ReverseLinkedListII_92.java
@@ -30,6 +30,24 @@ public class ReverseLinkedListII_92 {
             cur.next = temp.next;
             temp.next = pre.next;
             pre.next = temp;
+            /**
+             * 1-2-3-4
+             * pre = 1, cur = 2, temp = 3;
+             * temp = cur.next -> 3;
+             * 下一步要把现在 cur.next填上
+             * cur.next = temp.next; 因为要往后移动所以填temp.next; cur.next -> 4
+             * 继续填temp
+             * temp.next = pre.next; 把temp.next直接转换成pre.next; temp.next -> 2
+             * 1->  cur =  2-4-5 temp = 3 - 2
+             * pre.next = temp;
+             *
+             * 1-3-2-4-5
+             * pre = 1, cur = 2, temp = 3，不变但是顺序变了
+             * temp = cur.next -> 4
+             * cur.next = temp.next -> 2-5;
+             * temp.next = pre.next-> 4-3-2-5;
+             * pre.next = temp; 1-4-3-2-5;
+             */
         }
 
         return dummy.next;
--- src/TestCase.java
+++ src/TestCase.java
@@ -4,25 +4,4 @@ import java.util.*;
 
 public class TestCase {
 
-    private static int[] dp;
-
-    public static void main(String[] args) {
-        int[] nums = {10,9,2,5,3,7,101,18};
-        int[] dp = new int[nums.length];
-
-        int len = 0;
-        for (int num : nums) {
-            int i = Arrays.binarySearch(dp, 0, len, num);
-            System.out.println(i + ", nums: " + num);
-            if (i < 0) {
-                i = -(i + 1);
-            }
-            dp[i] = num;
-            if (i == len) {
-                len++;
-            }
-
-            System.out.println(len);
-        }
-    }
 }
