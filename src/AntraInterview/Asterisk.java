c269cd6dd6d3359993f6bda13c5ca3c68711e73d
TianyuanDeng <dty469783894@gmail.com>
2020-07-21 19:03:26 -0500

    Use Recursion

---
+++ src/AntraInterview/Asterisk.java
@@ -0,0 +1,27 @@
+package AntraInterview;
+
+public class Asterisk {
+    public static void main(String[] args) {
+        String s = "aabbcde";
+        StringBuilder sb = new StringBuilder();
+        recursion(s,0, 1, sb);
+        String result = sb.toString();
+        System.out.println(result);
+    }
+
+    public static void recursion (String s, int start, int end, StringBuilder result) {
+        if (end == s.length()) {
+            result.append(s.charAt(end - 1));
+            return;
+        }
+
+        for (int i = end; i < s.length(); i++) {
+            if (s.charAt(i) != s.charAt(i - 1)) {
+                String sub = s.substring(start, i) + "*";
+                result.append(sub);
+                recursion(s, i, i + 1, result);
+                break;
+            }
+        }
+    }
+}
